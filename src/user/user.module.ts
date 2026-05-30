9659f8de65d1758223c5b7fd7cd34079f0a30148
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-05-30 16:50:19 -0600

    feat: Se agrego la configuracion para el jwt en el modulo de los usuarios

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,29 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
   imports: [
     TypeOrmModule.forFeature([User]),
-  ]
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    })
+  ],
+  exports: [TypeOrmModule]
 })
 export class UserModule {}
