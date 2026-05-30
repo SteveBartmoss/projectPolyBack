a0ddd314eddd89f060ae968ba46d5e5df22d101c
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-05-30 16:49:35 -0600

    feat: Se agrega una ruta para el login de los usuarios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto){
+    return this.userService.login(loginUserDto)
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
