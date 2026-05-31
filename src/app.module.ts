2f206fe9dc73c55768fe61675d94325e30a34ba6
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-05-31 17:25:56 -0600

    feat: Se implemento la conexion a la base de datos de mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { UserModule } from './user/user.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    MongooseModule.forRoot(`mongodb://localhost:27017/${process.env.DB_MONGO_NAME}`),
     UserModule
   ],
   controllers: [AppController],
