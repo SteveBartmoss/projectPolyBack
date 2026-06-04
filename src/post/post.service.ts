db850442a53a5602a787c44581b93c50ccf41fce
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-06-03 19:12:09 -0600

    feat: Se implementa la creacion de un post

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Model } from 'mongoose';
+import { Post } from './entities/post.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+
+  constructor(
+
+    @InjectModel(Post.name)
+    private readonly postModel: Model<Post>
+  ){}
+
+  async create(createPostDto: CreatePostDto) {
+
+    try{
+      
+      const post = await this.postModel.create(createPostDto)
+
+      return post
+
+    } catch (error){
+      console.log(error)
+    }
+
   }
 
   findAll() {
