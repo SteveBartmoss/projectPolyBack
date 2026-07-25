63ef469926c3860fcc302d2ae927b3804bb1b29d
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-07-24 19:42:12 -0600

    feat: Se implemento el modulo para las colleciones de los usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostModule } from './post/post.module';
+import { CollectionUserModule } from './collecionUser/collection-user.module';
 
 @Module({
   imports: [
@@ -24,7 +25,8 @@ import { PostModule } from './post/post.module';
     }),
     MongooseModule.forRoot(`mongodb://localhost:27017/${process.env.DB_MONGO_NAME}`),
     UserModule,
-    PostModule
+    PostModule,
+    CollectionUserModule
   ],
   controllers: [AppController],
   providers: [AppService],
