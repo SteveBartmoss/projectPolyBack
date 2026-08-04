fb51f49f59819c92fd1f3a52642be51774b05f6d
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-03 18:56:52 -0600

    feat: Se implementa el entity de los archivos para el module

---
+++ src/files/entities/file.entity.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+
+@Schema({
+    timestamps: true
+})
+export class File extends Document {
+
+    @Prop({
+        required: true
+    })
+    path: string;
+
+    @Prop({
+        required: true
+    })
+    owner: string;
+
+    @Prop({
+        default: false
+    })
+    isPublic: boolean;
+
+    @Prop({
+        default: null
+    })
+    group: string;
+
+    @Prop({
+        required: true
+    })
+    originalName: string;
+
+    @Prop({
+        required: true,
+        type: Number
+    })
+    size: number;
+
+    @Prop({
+        required: true
+    })
+    mime: string;
+
+}
+
+export const FileSchema = SchemaFactory.createForClass(File)
