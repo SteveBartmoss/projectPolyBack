f81bf8390d2155cbef026a3ca0a6c212dfe05405
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-06 18:42:57 -0600

    feat: Se implemento la importacion del modulo de mongo

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { File, FileSchema } from './entities/file.entity';
 
 @Module({
   controllers: [FilesController],
   providers: [FilesService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: File.name,
+        schema: FileSchema
+      }
+    ]),
+  ],
 })
 export class FilesModule {}
